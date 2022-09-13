6368214af3460e557ca2b93f53c4ff8b66039739
Juan Acosta (acoju) <acosta.herrera.oliver@gmail.com>
2022-09-13 18:31:44 -0400

    Adding repositoy inferfaces

---
+++ ElegantRestaurant/Domain/Interfaces/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces
+{
+    public interface IRepository<T>
+    {
+        T Get(int id);
+        IEnumerable<T> Get(Expression<Func<T, bool>> predicate);
+        IEnumerable<T> GetAll();
+        IEnumerable<T> GetAll(Expression<Func<T, bool>> predicate);
+        T Add(T entity);
+        T Update(T entity);
+        T Delete(T entity);
+    }
+}
