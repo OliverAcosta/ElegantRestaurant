ca0503cbc1e461344e98c87681fca4e871dd64bb
Juan Acosta (acoju) <acosta.herrera.oliver@gmail.com>
2022-09-13 18:18:02 -0400

    adding files before git ignore file fix

---
+++ ElegantRestaurant/Infrastructure/Entities/Sales.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Entities
+{
+    [Table("Sales")]
+    public class Sales
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int ProductId { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public DateTime SellTime { get; set; }
+        public int StatesId { get; set; }
+
+        public States States { get; set; }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# ElegantRestaurant
+-created Base Solution
+-created Insfrastructure project
+-created Domain project
